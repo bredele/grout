5c89625e3bbd550dbfcc20cc6c1f64a5ded6277f
bredele <olivier.wietrich@gmail.com>
2015-01-27 20:46:55 -0700

    mix attributes and child nodes

--- index.js
+++ index.js
@@ -11,34 +11,31 @@ module.exports = x;
  * @api public
  */
 
-function x(tag, nodes) {
+function x(tag, attrs, nodes) {
   var dom = document.createElement(tag);
   return function() {
-    if(nodes instanceof Array) {
-      for(var i = 0, l = nodes.length; i < l; i++) {
-        var node = nodes[i];
-        if(typeof node === 'string') {
-          var text = document.createTextNode(node);
-          dom.appendChild(text);
-        } else {
-          dom.appendChild(node());
-        }
+    if(attrs instanceof Array) {
+      children(dom, attrs);
+    } else if(typeof attrs === 'object') {
+      for(var key in attrs) {
+        dom.setAttribute(key, attrs[key]);
       }
-    } else if(typeof nodes === 'object') {
-      for(var key in nodes) {
-        dom.setAttribute(key, nodes[key]);
-      }
+      if(nodes) children(dom, nodes);
     } else {
-      dom.innerHTML = nodes;
+      dom.innerHTML = attrs;
     }
     return dom;
   };
 }
 
-
-
-  // var dom = document.createElement(tag);
-  // for(var key in attrs) {
-  //  dom.setAttribute(key, attrs[key]);
-  // }
- //  return dom;
+function children(dom, nodes) {
+  for(var i = 0, l = nodes.length; i < l; i++) {
+    var node = nodes[i];
+    if(typeof node === 'string') {
+      var text = document.createTextNode(node);
+      dom.appendChild(text);
+    } else {
+      dom.appendChild(node());
+    }
+  }
+}
--- test/x.js
+++ test/x.js
@@ -8,96 +8,86 @@ var x = require('..');
 
 describe('basic', function() {
 
-	it('should return a function', function() {
-		var dom = x('button');
-		assert.equal(typeof dom, 'function');
-	});
+  it('should return a function', function() {
+    var dom = x('button');
+    assert.equal(typeof dom, 'function');
+  });
 
-	it('should create dom element with a specified tag', function() {
-		var el = x('button')();
-		assert.equal(el.nodeName, 'BUTTON');
-	});
+  it('should create dom element with a specified tag', function() {
+    var el = x('button')();
+    assert.equal(el.nodeName, 'BUTTON');
+  });
 
-	it('should set the inner value of the returned element', function() {
-		var el = x('button', 'hello')();
-		assert.equal(el.innerHTML, 'hello');
-	});
+  it('should set the inner value of the returned element', function() {
+    var el = x('button', 'hello')();
+    assert.equal(el.innerHTML, 'hello');
+  });
 
-	describe("inception", function() {
-		
-		it('should append one dom node', function() {
-			var dom = x('ul', [
-				x('li', 'hello')
-			])();
+  describe("inception", function() {
+    
+    it('should append one dom node', function() {
+      var dom = x('ul', [
+        x('li', 'hello')
+      ])();
 
-			var li = dom.firstChild;
-			assert.equal(li.nodeName, 'LI');
-			assert.equal(li.innerHTML, 'hello');
+      var li = dom.firstChild;
+      assert.equal(li.nodeName, 'LI');
+      assert.equal(li.innerHTML, 'hello');
 
-		});
+    });
 
-		it('should append multiple dom nodes', function() {
-			var dom = x('ul', [
-				x('li', 'hello'),
-				x('li', 'world')
-			])();
-			assert.equal(dom.children.length, 2);
-		});
+    it('should append multiple dom nodes', function() {
+      var dom = x('ul', [
+        x('li', 'hello'),
+        x('li', 'world')
+      ])();
+      assert.equal(dom.children.length, 2);
+    });
 
-		it('should append string as a dom node', function() {
-			var dom = x('ul', [
-				'hello'
-			])();
-			assert.equal(dom.innerHTML, 'hello');
-		});
+    it('should append string as a dom node', function() {
+      var dom = x('ul', [
+        'hello'
+      ])();
+      assert.equal(dom.innerHTML, 'hello');
+    });
 
-		it('should mix text and dom nodes', function() {
-			var dom = x('div', [
-				'hello',
-				x('ul', [
-					x('li', 'hello'),
-					x('li', 'world')
-				])
-			])();
-			assert.equal(dom.innerHTML, 'hello<ul><li>hello</li><li>world</li></ul>');
-		});
+    it('should mix text and dom nodes', function() {
+      var dom = x('div', [
+        'hello',
+        x('ul', [
+          x('li', 'hello'),
+          x('li', 'world')
+        ])
+      ])();
+      assert.equal(dom.innerHTML, 'hello<ul><li>hello</li><li>world</li></ul>');
+    });
 
-	});
-	
-	describe("attributes", function() {
-		
-		it('should set attributes', function() {
-			var dom = x('button', {
-				class: 'btn'
-			})();
-			assert.equal(dom.className, 'btn');
-		});
-		
-	});
-	
+  });
+  
+  describe("attributes", function() {
+    
+    it('should set attributes', function() {
+      var dom = x('button', {
+        class: 'btn'
+      })();
+      assert.equal(dom.className, 'btn');
+    });
 
+    it('should still set dom nodes', function() {
+      var dom = x('button', {
+        class: 'btn'
+      }, [
+        x('img', {
+          src: 'hello'
+        })
+      ])();
 
-	// describe('attributes', function() {
-
-	// 	it('should set attributes', function() {
-	// 		var dom = x('button', {
-	// 			class: 'btn'
-	// 		});
-	// 		assert.equal(dom.className, 'btn');
-	// 	});
-
-	// });
-
-	// describe('child nodes', function() {
-
-	// 	it('should append child nodes', function() {
-	// 		var list = x('ul', [
-	// 			x('li'),
-	// 			x('li')
-	// 		]);
-
-	// 	});
-
-	// });
+      var img = dom.firstChild;
+      assert.equal(img.nodeName, 'IMG');
+      assert.equal(img.getAttribute('src'), 'hello');
+    });
+    
+  });
+  
 
 });
