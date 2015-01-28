10e179a3279511a22f250dc9754b3f28c4d534f4
bredele <olivier.wietrich@gmail.com>
2015-01-27 20:14:47 -0700

    element inception

--- test/x.js
+++ test/x.js
@@ -18,10 +18,27 @@ describe('basic', function() {
 		assert.equal(el.nodeName, 'BUTTON');
 	});
 
-	it('should set the inner value of that dom', function() {
+	it('should set the inner value of the returned element', function() {
 		var el = x('button', 'hello')();
 		assert.equal(el.innerHTML, 'hello');
 	});
+
+	describe("inception", function() {
+		
+		it('should append child element', function() {
+			var dom = x('ul', [
+				x('li', 'hello')
+			])();
+
+			var li = dom.firstChild;
+			assert.equal(li.nodeName, 'LI');
+			assert.equal(li.innerHTML, 'hello');
+
+		});
+
+	});
+	
+
 
 	// describe('attributes', function() {
 
