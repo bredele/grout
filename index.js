0327a70766231024a1acfddea798a612e623cee3
bredele <olivier.wietrich@gmail.com>
2015-01-28 21:12:47 -0700

    first refactorin - pretty bad

--- index.js
+++ index.js
@@ -3,40 +3,46 @@
  * Expose 'grout'
  */
 
-module.exports = grout;
-
-
-/**
- * x constructor.
- * @api public
- */
-
-function grout(tag, attrs, nodes) {
+module.exports = function grout(tag, attrs, nodes) {
   var dom = document.createElement(tag);
+  var array = attrs instanceof Array;
+  var object = typeof attrs === 'object';
   return function(data) {
-    if(attrs instanceof Array) {
+    if(array) {
       children(dom, attrs);
-    } else if(typeof attrs === 'object') {
-      for(var key in attrs) {
-        var attr = attrs[key];
-        if(typeof attr === 'function') {
-          if(key.substring(0,2) === 'on') {
-            dom[key] = attr;
-            break;
-          } else {
-            attr = attr.call(data);
-          }
-        }
-        dom.setAttribute(key, attr);
-      }
+    } else if(object) {
+      attributes(dom, attrs, data);
       if(nodes) children(dom, nodes);
     } else {
       text(dom, attrs);
     }
     return dom;
   };
-}
+};
 
+
+/**
+ * Set attributes.
+ * 
+ * @param  {Element} dom
+ * @param  {Object} obj
+ * @param  {Object} data
+ * @api private
+ */
+
+function attributes(dom, obj, data) {
+  for(var key in obj) {
+    var attr = obj[key];
+    if(typeof attr === 'function') {
+      if(key.substring(0,2) === 'on') {
+        dom[key] = attr;
+        break;
+      }
+      attr = attr.call(data);
+    }
+    dom.setAttribute(key, attr);
+  }
+}
 
 
 /**
@@ -51,6 +57,7 @@ function text(dom, str) {
   dom.appendChild(document.createTextNode(str));
 }
 
+
 /**
  * Set node children elements.
  * 
@@ -60,17 +67,9 @@ function text(dom, str) {
  */
 
 function children(dom, nodes) {
-  if(typeof nodes === 'string') {
-    dom.appendChild(document.createTextNode(nodes));
-  } else {
-    for(var i = 0, l = nodes.length; i < l; i++) {
-      var node = nodes[i];
-      if(typeof node === 'string') {
-        var text = document.createTextNode(node);
-        dom.appendChild(text);
-      } else {
-        dom.appendChild(node());
-      }
-    }
+  for(var i = 0, l = nodes.length; i < l; i++) {
+    var node = nodes[i];
+    if(typeof node === 'string') text(dom, node);
+    else dom.appendChild(node());
   }
 }
