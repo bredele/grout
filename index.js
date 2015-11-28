0a72edb9af968d3149c459799d5fac2f85927586
bredele <olivier.wietrich@gmail.com>
2015-11-27 22:29:35 -0700

    add documentation

--- index.js
+++ index.js
@@ -5,6 +5,26 @@
 var tmpl = require('mouth');
 
 
+/**
+ * Virtual dom.
+ *
+ * Examples:
+ *
+ *   var fn = dom('button', 'hello ${name}', {
+ *     class: 'button ${type}'  
+ *   });
+ *
+ *   fn({name:'olivier', type: 'ctq'});
+ *   // => <button class="button cta">hello olivier</button>
+ *
+ *
+ * @param {String} tag
+ * @param {String|Array|Element?} content
+ * @param {Object?} attrs
+ * @return {Function}
+ * @api public
+ */
+
 module.exports = function(tag, content, attrs) {
   var el = document.createElement(tag);
   return function(data) {
@@ -15,6 +35,15 @@ module.exports = function(tag, content, attrs) {
   };
 };
 
+
+/**
+ * Render virtual dom content.
+ *
+ * @param {String|Array|Element} content
+ * @param {Object?} content
+ * @return {Element}
+ * @api private
+ */
 
 function render(content, data) {
   var type = typeof content;
@@ -29,6 +58,14 @@ function render(content, data) {
 }
 
 
+/**
+ * Render fragment of virtual dom.
+ *
+ * @param {Array} arr
+ * @return {DocumentFragment}
+ * @api private
+ */
+
 function fragment(arr) {
   var el = document.createDocumentFragment();
   for(var i = 0, l = arr.length; i < l; i++) {
@@ -38,6 +75,15 @@ function fragment(arr) {
 }
 
 
+/**
+ * Render virtual dom attributes.
+ *
+ * @param {Element} ell
+ * @param {Object} attrs
+ * @param {Object} dat
+ * @api private
+ */
+
 function attributes(el, attrs, data) {
   for(var key in attrs) {
     var value = attrs[key];
@@ -45,12 +91,22 @@ function attributes(el, attrs, data) {
     else if(typeof value === 'function') {
       var bool = key.substring(0, 2) === 'on';
       if(bool) el.addEventListener(key.slice(2), value);
+      // @not we should compute the function if it's not an event
       break;
     }
     // @note should refactor with render (too bad attribute can't append text node anymore)
     el.setAttribute(key,  tmpl(value, data)[0](data));
   }
 }
+
+
+/**
+ * Render virtual dom styles.
+ *
+ * @param {Object} obj
+ * @return {String}
+ * @api private
+ */
 
 function styles(obj) {
   var str = '';
