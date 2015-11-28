c7f490bf9bb5205de9145f4c68aa64be91a8a7e9
bredele <olivier.wietrich@gmail.com>
2015-11-27 20:10:36 -0700

    append existing DOM element

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 module.exports = function(tag, content) {
   var el = document.createElement(tag);
   return function(data) {
-    el.innerText = content;
+    if(typeof content === 'string') el.innerText = content;
+    else if(content) el.appendChild(content);
     return el;
   };
 };
--- test/grout.js
+++ test/grout.js
@@ -12,14 +12,20 @@ describe('basic', function() {
 		assert.equal(typeof dom, 'function');
 	});
 
-	it('should create an element', function() {
+	it('should create a DOM element', function() {
 		var el = dom('h1')();
 		assert.equal(el.nodeName, 'H1');
 	});
 
-	it('should set inner text of an element', function() {
+	it('should set inner text of a DOM element', function() {
 		var el = dom('span', 'hello world')();
 		assert.equal(el.innerText, 'hello world');
+	});
+
+	it('should append DOM element', function() {
+		var span = document.createElement('span');
+		var el = dom('a', span)();
+		assert.equal(el.firstChild, span);
 	});
 
 });
